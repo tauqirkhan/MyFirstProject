c2af30e231cbd4f73009cf22e28a06cb4891d475
Tauqir Khan <tauqirkhann.11@gmail.com>
2025-05-22 02:47:16 +0530

    feat: Banking Program

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,75 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main{
-    public static void main(String[] args){
-        //Number guessing game
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
 
-        int guess;
-        int attempts = 0;
-        int min = 0;
-        int max = 100;
-        int randomNumber = random.nextInt(min, max + 1);
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Number Guessing Game");
-        System.out.printf("Guess a number between %d-%d\n", min, max);
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+        boolean shownOption = true;
 
-        do{
-            System.out.print("Enter a guess: ");
-            guess =  scanner.nextInt();
-            attempts++;
-
-            if(guess < randomNumber){
-                System.out.println("TOO LOW! Try again");
-            } else if (guess > randomNumber){
-                System.out.println("TOO HIGH! Try again");
-            } else{
-                System.out.printf("Correct! The number was %d\n", guess);
-                System.out.printf("# of attempts %d\n",attempts);
+        while(isRunning){
+            if(shownOption){
+                System.out.println("***************");
+                System.out.println("BANKING PROGRAM");
+                System.out.println("***************");
+                System.out.println("1. Show Balance");
+                System.out.println("2. Deposit");
+                System.out.println("3. Withdraw");
+                System.out.println("4. Exit");
+                System.out.println("***************");
             }
-        } while(randomNumber != guess);
 
-        scanner.close();
+            shownOption = false;
+            System.out.print("Enter your choice (1 - 4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid input");
+            }
+        }
+
+        System.out.println("***************************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************************");
+    }
+
+    static void showBalance(double balance){
+        System.out.println("***************************");
+        System.out.printf("Your current balance is $%.2f\n", balance);
+        System.out.println("***************************");
+    }
+
+    static double deposit(){
+        double deposit;
+
+        System.out.print("Enter the deposit amount: ");
+        deposit = scanner.nextDouble();
+
+        return deposit;
+    }
+
+    static double withdraw(double balance){
+        double amount = 0;
+
+        System.out.print("Enter the amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if(amount > balance){
+            System.out.println("You cannot withdraw, greater then balance");
+            return 0;
+        } else if(amount < 0){
+            System.out.println("Withdrawn amount cannot be negative");
+            return  0;
+        } else {
+            return amount;
+        }
     }
 }
