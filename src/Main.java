6ff0820dd75d4f2dd9444d38cc65c783261eb54a
Tauqir Khan <tauqirkhann.11@gmail.com>
2025-05-23 08:41:38 +0530

    feat: candy slot machine

--- src/Main.java
+++ src/Main.java
@@ -5,70 +5,117 @@ public class Main{
 
     public static void main(String[] args) {
 
-        // ROCK PAPER SCISSORS GAME
+        // CANDY SLOT MACHINE
 
         Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+        String[] options = {"🍬", "🍭", "🍫", "🫕", "🍪"} ;
+        char playAgain = 'y';
+
+        System.out.println("****************************************************************");
+        System.out.println("  Welcome To Candy Slot Machine, Where You'll Risk Your Candy.  ");
+        System.out.println("Symbols: 🍬 | 🍭 | 🍫 | 🫕 | 🍪 ");
+        System.out.println("****************************************************************");
+
+        while (playAgain == 'y'){
+            if(balance > 0){
+
+                System.out.print("Enter bet amount of candies: ");
+                bet = scanner.nextInt();
+
+                if(bet > balance){
+                    System.out.printf("Your current candies balance are %d,\nAmount can't be more than candies balance\n", balance);
+                    //skip
+                    continue;
+                }
+                if(bet <= 0){
+                    System.out.println("Candy bet amount can't be negative or 0");
+                    //skip
+                    continue;
+                }
+
+                balance -= bet;
+
+                System.out.println("Spinning...");
+                row = spinRow(options);
+
+                for(String symbol: row){
+                    System.out.println(symbol);
+                }
+
+                payout = getPayoutAmount(row, bet);
+                if(payout > 0){
+                    System.out.printf("You won %d candies!\n", payout);
+                    balance += payout;
+                } else{
+                    System.out.printf("You lost %d candies\n", bet);
+                }
+
+                System.out.printf("Your current candy amount is %d\n", balance);
+            }
+            // ASK TO PLAY AGAIN
+            System.out.print("Do you want to play again? (y/n)");
+            playAgain = Character.toLowerCase(scanner.next().charAt(0));
+        }
+
+        System.out.println("*****************");
+        System.out.println("Thanks For Playing!");
+        System.out.println("*****************");
+    }
+
+    static int getPayoutAmount(String[] row, int bet){
+        if (row[0].equals(row[1]) && row[1].equals(row[2])) {
+            return switch (row[0]) {
+                case "🍬" -> bet * 4;
+                case "🍭" -> bet * 6;
+                case "🍫" -> bet * 8;
+                case "🫕" -> bet * 10;
+                case "🍪" -> bet * 20;
+                default -> 0;
+            };
+        } else if(row[0].equals(row[1])){
+            return switch(row[1]){
+                case "🍬" -> bet * 2;
+                case "🍭" -> bet * 3;
+                case "🍫" -> bet * 4;
+                case "🫕" -> bet * 5;
+                case "🍪" -> bet * 10;
+                default -> 0;
+            };
+        } else if (row[1].equals(row[2])){
+            return switch(row[0]){
+                case "🍬" -> bet * 2;
+                case "🍭" -> bet * 3;
+                case "🍫" -> bet * 4;
+                case "🫕" -> bet * 5;
+                case "🍪" -> bet * 10;
+                default -> 0;
+            };
+        }else if (row[0].equals(row[2])) {
+            return switch (row[0]) {
+                case "🍬" -> bet * 2;
+                case "🍭" -> bet * 3;
+                case "🍫" -> bet * 4;
+                case "🫕" -> bet * 5;
+                case "🍪" -> bet * 10;
+                default -> 0;
+            };
+        } else {
+            return 0;
+        }
+    }
+
+    static String[] spinRow(String[] options){
+        String[] row = new String[3];
         Random random = new Random();
 
-        String[] choices = {"rock", "paper", "scissors"};
-        String playerChoice;
-        String computerChoice;
-        int[][] yourWinningChoices = {
-                {0, 2},
-                {1, 0},
-                {2, 1},
-        };
-        char playAgain = 'n';
+        for(int i = 0; i < 3; i++){
+              row[i] = options[random.nextInt(options.length)];
+        }
 
-        System.out.println("*************************");
-        System.out.println("Rock, Paper, Scissor Game");
-        System.out.println("*************************");
-
-        do{
-            System.out.print("Enter the your choice? (rock, paper, scissor) ");
-            playerChoice = scanner.nextLine().toLowerCase();
-
-            computerChoice = choices[random.nextInt(3)];
-            System.out.printf("Computer Choice is %s. \n", computerChoice);
-
-            if(computerChoice.equals(playerChoice)){
-                System.out.println("It's a tie!");
-                playAgain = 'n';
-            } else if (
-                    (playerChoice.equals(choices[yourWinningChoices[0][0]]) && computerChoice.equals(choices[yourWinningChoices[0][1]])) ||
-                            (playerChoice.equals(choices[yourWinningChoices[1][0]]) && computerChoice.equals(choices[yourWinningChoices[1][1]])) ||
-                            (playerChoice.equals(choices[yourWinningChoices[2][0]]) && computerChoice.equals(choices[yourWinningChoices[2][1]]))
-            ) {
-                System.out.println("*********");
-                System.out.println("You Won!!!");
-                System.out.println("*********");
-                playAgain = 'n';
-            } else{
-                if(!playerChoice.equals(choices[0]) && !playerChoice.equals(choices[1]) && !playerChoice.equals(choices[2])){
-                    System.out.println("Invalid choice!");
-                    playAgain = 'y';
-                    continue;
-                } else {
-                    System.out.println("*********");
-                    System.out.println("You Lost!!!");
-                    System.out.println("*********");
-                    playAgain = 'n';
-                }
-            }
-
-            System.out.print("Want to play again? (y/n)");
-             if(Character.toLowerCase(scanner.next().charAt(0)) == 'y'){
-                 playAgain = 'y';
-             } else {
-                 playAgain = 'n';
-             };
-
-            //To clear buffer created from scanner.next()
-             scanner.nextLine();
-        }while (playAgain == 'y');
-
-        System.out.println("*****************");
-        System.out.println("Thanks For Playing!!!");
-        System.out.println("*****************");
+        return  row;
     }
 }
